a2f17febde4392abd9aaee40cda265f4a97385ed
WhiteSnake <whitesnake35711@gmail.com>
2024-12-26 16:08:01 +0530

    created basic folder structure for achievement management

--- src/achievement/achievement.module.ts
+++ src/achievement/achievement.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AchievementController } from './achievement.controller';
 import { AchievementService } from './achievement.service';
+import { AchievementsModule } from './achievements/achievements.module';
+import { AnnouncementsModule } from './announcements/announcements.module';
+import { RequestsModule } from './requests/requests.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   controllers: [AchievementController],
-  providers: [AchievementService]
+  providers: [AchievementService],
+  imports: [AchievementsModule, AnnouncementsModule, RequestsModule, NotificationsModule]
 })
 export class AchievementModule {}
