dbf8cd96f250d661caf563f95df725df4ed9490e
Mohit Gusain <mohitgusain8671@gmail.com>
2025-01-02 22:06:31 +0530

    Creating Directory Structure for Alumni and adding userservices and insert job table

---
+++ src/alumni/users/users.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto, UpdateUserDto } from './dto/index'
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+    constructor(private prisma: PrismaService) {}
+
+    async create(dto: CreateUserDto) {
+        let item: any;
+            item = await this.prisma.user.findFirst({
+              where: {
+                enrollmentNumber: dto.enrollmentNumber,
+                email: dto.email,
+                mobile: dto.mobile,
+              },
+            });
+            if (item)
+              return { status: 'error', message: 'user already exists!' };
+            item = await this.prisma.user.create({
+              data: dto
+            }) ;
+            return {
+              status: 'success',
+              item,
+              message: 'User added successfully',
+            };
+    }
+
+    findAll() {
+        return this.prisma.user.findMany();
+    }
+
+    findOne(id: number) {
+        return this.prisma.user.findUnique({ where: { userId: id, } });
+    }
+
+    update(id: number, Dto: UpdateUserDto) {
+        return this.prisma.user.update({
+            where: { userId: id },
+            data: Dto,
+        });
+    }
+
+    remove(id: number) {
+        return this.prisma.user.delete({ where: { userId: id } });
+    }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { SocietyModule } from './society/society.module';
 import { AchievementModule } from './achievement/achievement.module';
+import { AlumniModule } from './alumni/alumni.module';
 
 @Module({
-  imports: [PrismaModule, SocietyModule, AchievementModule],
+  imports: [PrismaModule, SocietyModule, AchievementModule, AlumniModule],
 })
 export class AppModule {}
